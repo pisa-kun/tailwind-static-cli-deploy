8899e9bd8194ba2eb7e61e205af367b648ecec6b
pisa <pisa0401mst@gmail.com>
2022-11-27 19:20:07 +0900

    static s3 bucket tailwind

---
+++ README.md
@@ -0,0 +1,11 @@
+# tailwindcss 初期化 & AWS CLI deploy
+
+## 手順
+
+1. mkdir tailwind && cd tailwind
+2. npm init -y
+3. npm install -D tailwindcss
+4. npx tailwindcss init
+5. ファイル修正
+6. npx tailwindcss -i ./src/input.css -o ./dist/output.css
+7.  src/index.htmlの作成
---
+++ make.bat
@@ -0,0 +1,13 @@
+npx tailwindcss -i ./src/input.css -o ./dist/output.css
+
+aws s3 mb s3://tailwind-static-cli-deploy
+
+aws s3api put-public-access-block --bucket tailwind-static-cli-deploy --public-access-block-configuration  "BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false"
+
+aws s3api put-bucket-policy --bucket tailwind-static-cli-deploy --policy file://policy/bucket-policy.json
+
+aws s3 cp src/index.html s3://tailwind-static-cli-deploy/index.html
+aws s3 cp dist/output.css s3://tailwind-static-cli-deploy/dist/output.css
+aws s3 cp img/akinze.png s3://tailwind-static-cli-deploy/img/akinze.png
+
+aws s3 website s3://tailwind-static-cli-deploy --index-document index.html
